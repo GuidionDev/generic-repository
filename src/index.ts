6bfc18b80e864bdbcf2a4e92909dcebc1ffba2ab
Fons Eppink <fonseppink@hotmail.com>
2018-02-26 11:37:50 +0100

    fix: make NotFoundError publicly available

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import MongoDbRepository from './implementations/mongodb_repository';
 import Seed from './implementations/seed';
 import MemoryRepository from './implementations/memory_repository';
 import MongoDbConnect from './mongodb_connect';
+import { NotFoundError } from './errors/not_found_error'
 
 export {
   Repository,
   MongoDbRepository,
   MemoryRepository,
   MongoDbConnect,
-  Seed
+  Seed,
+  NotFoundError,
 };
