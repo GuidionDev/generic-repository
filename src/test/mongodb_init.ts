4d411350929f3367c0617888cc2c721f1cd81641
Rene Slump <rslump@outlook.com>
2018-10-09 13:57:25 +0200

    test: use env settings for tests

--- src/test/mongodb_init.ts
+++ src/test/mongodb_init.ts
@@ -6,12 +6,13 @@ import * as dotEnv from 'dotenv';
 // Load config setings
 dotEnv.config();
 process.env.NODE_ENV = 'test';
-
+process.env.DB_URI_TEST = 'mongodb://localhost:27017/';
+process.env.DB_NAME_TEST = 'test';
 export const connection = MongoConnection.connect();
 export const repo = new MongoDBRepository(SomeObject, connection);
 export const anotherRepo = new MongoDBRepository(AnotherObject, connection);
 export default repo;
 
-after(function() {
+after(function () {
   MongoConnection.gracefulExit();
 });
